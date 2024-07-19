5ab2f6e6859ea2bdb20d12c1023e41246ec75dbd
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2024-07-19 06:11:46 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// var head = '+998';
+// var userNumber = prompt('raqamingizni kiriting +998 xxxxxxx');
+
+
+
+// for (let i = 0; i < 500; i++) {
+//     var begin = (Math.floor(Math.random()*9) + 90)+'';
+//     var middle = Math.floor(Math.random()*100) + 100;
+//     var thirh = Math.floor(Math.random()*91) + 9;
+//     var fourth = Math.floor(Math.random()*91) + 9;
+//     var result = begin + middle + thirh + fourth;
+//     if(result == userNumber){
+//         console.log(true);
+
+//     }
+//     else{
+//         console.log(false);
+//     }
+// }
+
+var sixNum = prompt("enter six number like 123456")
+for (let i = 0; i < 30; i++) {
+    var ranNum = Math.floor(Math.random() * 300);
+    console.log(ranNum);
+}
+var arr = [];
+arr[length] = ranNum;
+console.log(arr);
+
+
+
+
+
+
+
+
+
+// 
---
+++ work2/scritp.js
@@ -0,0 +1,19 @@
+var color1 = Math.floor(Math.random() * 255);
+var color2 = Math.floor(Math.random() * 255);
+var color3 = Math.floor(Math.random() * 255);
+
+var color4 = Math.floor(Math.random() * 255);
+var color5 = Math.floor(Math.random() * 255);
+var color6 = Math.floor(Math.random() * 255);
+
+var color7 = Math.floor(Math.random() * 255);
+var color8 = Math.floor(Math.random() * 255);
+var color9 = Math.floor(Math.random() * 255);
+
+
+var deg = Math.floor(Math.random()* 360);
+
+var result =  'rgb(' + color1 + ',' + color2 + ',' + color3 +'),';
+var result2 =  'rgb(' + color4 + ',' + color5 + ',' + color6 +'),';
+var result3 =  'rgb(' + color7 + ',' + color8 + ',' + color9 +')';
+console.log(result, result2, result3);
